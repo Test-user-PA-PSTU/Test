f1b98c04f4fafea8e7b109e53210360b337b10e0
Test-user-PA-PSTU <dsvgdfbfdbfdbd222@mail.ru>
2024-10-17 15:57:14 +0500

    Add files via upload

---
+++ Vopros3/Vopros3/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] values = { null, "160519", "9432.0", "16,667",
+                          "   -322   ", "+4302", "(100);", "01FA" };
+foreach (var value in values)
+{
+    int number;
+
+    bool success = int.TryParse(value, out number);
+    if (success)
+    {
+        Console.WriteLine($"Converted '{value}' to {number}.");
+    }
+    else
+    {
+        Console.WriteLine($"Attempted conversion of '{value ?? "<null>"}' failed.");
+    }
+}
